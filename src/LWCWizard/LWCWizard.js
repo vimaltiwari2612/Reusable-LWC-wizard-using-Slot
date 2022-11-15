5b9d10da7c51ddf455cc56053a2f81d618d689ed
Vimal Tiwari <vimaltiwari2612@gmail.com>
2022-11-15 16:29:26 +0530

    Update LWCWizard.js

--- src/LWCWizard/LWCWizard.js
+++ src/LWCWizard/LWCWizard.js
@@ -1,75 +1,60 @@
 import {LightningElement, track} from 'lwc';
 
-export default class LWCWizard extends LightningElement {
+export default class CpqQuoteSetupWizard extends LightningElement {
   @track
-  currentStep = '0';
+  currentStep = '1';
   @track
   pages = [];
-  componentsRegistered = 0;
-  privateChildrenRecord = [];
-  isWizardSet = false;
 
-  @track
-  componentVisibilityMatrix = {
-    showFirst: true,
-    showSecond: true,
-    showThird: true,
-    showForth: true,
-    showFifth: true,
-    showSixth: true,
+  SHOW_FRAME = '';
+  HIDE_FRAME = 'slds-hide';
+
+  validate() {
+    let currentScreen = this.getCurrentScreen();
+    return (currentScreen) ? currentScreen.validate() : true;
   }
 
-  updateComponentVisibility() {
-    this.componentVisibilityMatrix.showFirst = false;
-    this.componentVisibilityMatrix.showSecond = false;
-    this.componentVisibilityMatrix.showThird = false;
-    this.componentVisibilityMatrix.showForth = false;
-    this.componentVisibilityMatrix.showFifth = false;
-    this.componentVisibilityMatrix.showSixth = false;
-
-    if (this.currentStep == '1') this.componentVisibilityMatrix.showFirst = true;
-    if (this.currentStep == '2') this.componentVisibilityMatrix.showSecond = true;
-    if (this.currentStep == '3') this.componentVisibilityMatrix.showThird = true;
-    if (this.currentStep == '4') this.componentVisibilityMatrix.showForth = true;
-    if (this.currentStep == '5') this.componentVisibilityMatrix.showFifth = true;
-    if (this.currentStep == '6') this.componentVisibilityMatrix.showSixth = true;
+  updateCurrentFrameVisibility(className) {
+    let currentScreen = this.getCurrentScreen();
+    if (currentScreen) currentScreen.className = className;
   }
 
-  setPageCount(count) {
-    let i = 1;
-    while (i <= count) {
-      this.pages.push({label: i.toString(), value: i.toString()});
-      i++;
+  getAllScreens() {
+    return this.querySelectorAll('*');
+  }
+
+  getCurrentScreen() {
+    if (this.pages) {
+      let index = parseInt(this.currentStep) - 1;
+      return this.pages[index].component;
     }
-    if (this.pages.length > 0) {
-      this.currentStep = '1';
+    return null;
+  }
+
+  populateScreensData() {
+    if (this.pages) {
+      this.getAllScreens().forEach(item => {
+        if (item.frame) {
+          this.pages.push({label: item.name, value: item.frame, component: item});
+        }
+      });
     }
   }
 
-  handleChildRegister(event) {
-    const item = event.detail;
-    const guid = item.guid;
-    this.privateChildrenRecord[guid] = item;
-    this.componentsRegistered++;
-    // Add a callback that
-    // notifies the parent when child is unregistered
-    item.callbacks.registerDisconnectCallback(this.handleChildUnregister);
-  }
-
-  handleChildUnregister(event) {
-    const item = event.detail;
-    const guid = item.guid;
-    this.privateChildrenRecord[guid] = undefined;
-    this.componentsRegistered--;
-  }
-
-  renderedCallback() {
-    if (!this.isWizardSet) {
-      this.setPageCount(this.componentsRegistered);
-      this.isWizardSet = true;
-      this.updateComponentVisibility();
+  hideAllScreens() {
+    if (this.pages) {
+      this.pages.forEach(item => {
+        item.component.className = this.HIDE_FRAME;
+      })
     }
   }
+
+  handleSlotChange() {
+    this.populateScreensData();
+    this.hideAllScreens();
+    this.updateCurrentFrameVisibility(this.SHOW_FRAME);
+  }
+
 
   get isEnableNext() {
     return parseInt(this.currentStep) != this.pages.length;
@@ -79,21 +64,24 @@ export default class LWCWizard extends LightningElement {
     return parseInt(this.currentStep) > 1;
   }
 
-  get isEnableFinish() {
+  get isLastScreen() {
     return parseInt(this.currentStep) === this.pages.length;
   }
 
   handleNext() {
+    if (!this.validate()) return;
+    this.updateCurrentFrameVisibility(this.HIDE_FRAME);
     this.currentStep = (parseInt(this.currentStep) + 1).toString();
-    this.updateComponentVisibility();
+    this.updateCurrentFrameVisibility(this.SHOW_FRAME);
   }
 
   handlePrev() {
+    this.updateCurrentFrameVisibility(this.HIDE_FRAME);
     this.currentStep = (parseInt(this.currentStep) - 1).toString();
-    this.updateComponentVisibility();
+    this.updateCurrentFrameVisibility(this.SHOW_FRAME);
   }
 
-  handleFinish() {
-    this.updateComponentVisibility();
+  handleValidate() {
+    this.validate();
   }
 }
