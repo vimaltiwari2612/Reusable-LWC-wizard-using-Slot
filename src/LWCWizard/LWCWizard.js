62ebb44e85304a0ab431bb2cb16ebdd976b60db1
Vimal Tiwari <vimaltiwari2612@gmail.com>
2022-11-16 10:58:47 +0530

    Update LWCWizard.js

--- src/LWCWizard/LWCWizard.js
+++ src/LWCWizard/LWCWizard.js
@@ -1,6 +1,6 @@
 import {LightningElement, track} from 'lwc';
 
-export default class CpqQuoteSetupWizard extends LightningElement {
+export default class LWCWizard extends LightningElement {
   @track
   currentStep = '1';
   @track
