9f3cedab1bb1753762580fd52a7c9326878acabe
Vimal Tiwari <vimaltiwari2612@gmail.com>
2022-11-16 10:56:35 +0530

    Update testChildLwc.js

--- src/testChildLwc/testChildLwc.js
+++ src/testChildLwc/testChildLwc.js
@@ -1,3 +1,4 @@
+import LightningAlert from 'lightning/alert';
 import {api, LightningElement, track} from 'lwc';
 
 export default class TestChildLwc extends LightningElement {
@@ -5,9 +6,26 @@ export default class TestChildLwc extends LightningElement {
   frame;
   @api
   name;
+  @track
+  text;
 
-  @api validate(){
-    return true
+  handleChange(event) {
+    this.text = event.target.value;
   }
 
+  sendData() {
+    const ev = new CustomEvent('sharedata', {detail: {data: this.text, frame: this.frame}});
+    this.dispatchEvent(ev);
+  }
+
+  @api
+  validate() {
+    if (this.text) {
+      this.sendData();
+      return true;
+    }
+    LightningAlert.open(
+        {message: 'Please provide all Inputs for ' + this.name, theme: 'error', label: 'Error'});
+    return false;
+  }
 }
